1467eff72f304ae84d237b834ac2a385f2f6ad28
RainySorcerer <beastdark704@gmail.com>
2025-07-29 15:28:16 +0530

    nabu: Build android.hardware.audio.sounddose-vendor-impl
    07-29 03:07:25.162  2702  2702 E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library android.hardware.audio.sounddose-vendor-impl.so not found
    07-29 03:07:25.162  2702  2702 W audiohalservice: createISoundDoseFactory() from android.hardware.audio.sounddose-vendor-impl failed
    
    Co-authored-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>
    Signed-off-by: RainySorcerer <beastdark704@gmail.com>

--- device.mk
+++ device.mk
@@ -159,6 +159,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
